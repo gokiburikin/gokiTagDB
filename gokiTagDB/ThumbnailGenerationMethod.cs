8919132f6fd5608dfe04523e7383ae42ebdeb24e
gokiburikin <gokiburikin@gmail.com>
2015-05-18 05:42:33 -0400

    Forgot a file

---
+++ gokiTagDB/ThumbnailGenerationMethod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gokiTagDB
+{
+    public enum ThumbnailGenerationMethod
+    {
+        JPEG, PNG, Auto, Smart
+    }
+}
